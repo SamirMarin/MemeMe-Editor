967e520864da09dea051c6efc4f8c24e176562ca
Samir Marin <marin.samir@gmail.com>
2015-12-09 00:40:26 -0800

    added camera button option, and Top and Bottom text fields with MemeTextEditDelegate extening UITextFieldDelegate the delegate for both text fields

--- MeMeEditor/ViewController.swift
+++ MeMeEditor/ViewController.swift
@@ -9,16 +9,37 @@
 import UIKit
 
 class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
-
+    @IBOutlet weak var cameraButton: UIBarButtonItem!
     @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var topTextField: UITextField!
+    @IBOutlet weak var bottomTextField: UITextField!
+    
+    let memeTextEditDelegate = MemeTextEditDelegate()
     override func viewDidLoad() {
         super.viewDidLoad()
+        cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
         
+        topTextField.text = "TOP"
+        bottomTextField.text = "BOTTOM"
+        
+        //setting text field delegates
+        topTextField.delegate = memeTextEditDelegate
+        bottomTextField.delegate = memeTextEditDelegate
+        //the current text is set by default
+        memeTextEditDelegate.isItDefaultTextTop = true
+        memeTextEditDelegate.isItDefaultTextBottom = true
     }
     @IBAction func selectImage(sender: UIBarButtonItem) {
         let pickImageController = UIImagePickerController()
         pickImageController.delegate = self
         self.presentViewController(pickImageController, animated: true, completion: nil)
+    }
+    
+    @IBAction func takePhoto(sender: UIBarButtonItem) {
+        let pickImageController = UIImagePickerController()
+        pickImageController.delegate = self
+        self.presentViewController(pickImageController, animated: true, completion: nil)
+        
     }
     func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
         dismissViewControllerAnimated(true, completion: nil)
---
+++ MemeTextEditDelegate.swift
@@ -0,0 +1,40 @@
+//
+//  MemeTextEditDelegate.swift
+//  MeMeEditor
+//
+//  Created by Samir Marin on 2015-12-08.
+//  Copyright © 2015 Samir Marin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MemeTextEditDelegate: NSObject, UITextFieldDelegate{
+    
+    var isItDefaultTextTop: Bool!
+    var isItDefaultTextBottom: Bool!
+    
+    func textField(textField: UITextField, shouldChangeCharactersInRange range: NSRange, replacementString string: String) -> Bool {
+        return true
+    }
+    
+    func textFieldDidBeginEditing(textField: UITextField) {
+        if let isDefualt = isItDefaultTextTop{
+            if(isDefualt && textField.text == "TOP"){
+                textField.text = ""
+                isItDefaultTextTop = false
+            }
+        }
+        if let isDefault = isItDefaultTextBottom{
+            if(isDefault && textField.text == "BOTTOM"){
+                textField.text = ""
+                isItDefaultTextBottom = false
+            }
+        }
+    }
+
+    func textFieldShouldReturn(textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+}
