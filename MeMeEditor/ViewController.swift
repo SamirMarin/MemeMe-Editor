2fed911cb2a7f6c0bf3541c7ca114d8d6f76378d
Samir Marin <marin.samir@gmail.com>
2015-12-10 01:48:28 -0800

    added textBox attributes... and functunality to move bottom text box up so it's not coverd by keyboard

--- MeMeEditor/ViewController.swift
+++ MeMeEditor/ViewController.swift
@@ -14,7 +14,15 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     @IBOutlet weak var topTextField: UITextField!
     @IBOutlet weak var bottomTextField: UITextField!
     
+    //Text attributes dictinary:
+    let memeTextAttributes = [
+        NSStrokeColorAttributeName: UIColor.blackColor(),
+        NSForegroundColorAttributeName: UIColor.whiteColor(),
+        NSFontAttributeName: UIFont(name: "HelveticaNeue-CondensedBlack", size: 40)!,
+        NSStrokeWidthAttributeName: NSNumber(float: 3)]
+    
     let memeTextEditDelegate = MemeTextEditDelegate()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         cameraButton.enabled = UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera)
@@ -28,16 +36,36 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
         //the current text is set by default
         memeTextEditDelegate.isItDefaultTextTop = true
         memeTextEditDelegate.isItDefaultTextBottom = true
+        
+        //set text field attributes
+        topTextField.defaultTextAttributes = memeTextAttributes
+        bottomTextField.defaultTextAttributes = memeTextAttributes
+        
+        //center the text
+        topTextField.textAlignment = NSTextAlignment.Center
+        bottomTextField.textAlignment = NSTextAlignment.Center
+    }
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        subscribeToKeyBoardNotifications()
+        
+    }
+    override func viewWillDisappear(animated: Bool) {
+        super.viewWillDisappear(animated)
+        unSubscribeToKeyBoardNotifications()
+        
     }
     @IBAction func selectImage(sender: UIBarButtonItem) {
         let pickImageController = UIImagePickerController()
         pickImageController.delegate = self
+        pickImageController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
         self.presentViewController(pickImageController, animated: true, completion: nil)
     }
     
     @IBAction func takePhoto(sender: UIBarButtonItem) {
         let pickImageController = UIImagePickerController()
         pickImageController.delegate = self
+        pickImageController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
         self.presentViewController(pickImageController, animated: true, completion: nil)
         
     }
@@ -51,6 +79,22 @@ class ViewController: UIViewController, UIImagePickerControllerDelegate, UINavig
     }
     func imagePickerControllerDidCancel(picker: UIImagePickerController) {
         dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func keyBoardWillShow(notification: NSNotification){
+        view.frame.origin.y -= heightOfKeyBoard(notification)
+    }
+    func heightOfKeyBoard(notification: NSNotification)->CGFloat{
+        let userInfo = notification.userInfo
+        let keyBoardSize = userInfo![UIKeyboardFrameEndUserInfoKey] as! NSValue
+        return keyBoardSize.CGRectValue().height
+    }
+    
+    func subscribeToKeyBoardNotifications(){
+        NSNotificationCenter.defaultCenter().addObserver(self, selector: "keyBoardWillShow:", name: UIKeyboardWillShowNotification, object: nil)
+    }
+    func unSubscribeToKeyBoardNotifications(){
+        NSNotificationCenter.defaultCenter().removeObserver(self, name: UIKeyboardWillShowNotification, object: nil)
     }
 
 }
